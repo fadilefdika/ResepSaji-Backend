cac3f963215b8b9c85b2543b7e9363dcce1cf391
uvenven <fadilefd1102@gmail.com>
2024-10-16 20:59:04 +0700

    feat: create Filament resources to add API key from admin feat: create model and migration for saving name and API key feat: create middleware for API key validation feat: add CORS support (not yet utilized) fix: wrap routes with middleware to require API key for access

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias(['api_key'=>\App\Http\Middleware\CheckApiKey::class,]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
